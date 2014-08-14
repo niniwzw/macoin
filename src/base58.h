0ff46100f5cfd753d482301bf3c5a6732d124851
niniwzw <vipwzw@vip.qq.com>
2014-08-14 23:03:31 +0800

    address same to bitcoin

--- src/base58.h
+++ src/base58.h
@@ -276,8 +276,8 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 25,
-        SCRIPT_ADDRESS = 85,
+        PUBKEY_ADDRESS = 0,
+        SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
     };
